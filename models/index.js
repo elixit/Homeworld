3d6f8976b5dfb2e2eb2dd9589d5ead62129b8553
Sherwin Abraham <sherwinabraham@gmail.com>
2023-08-24 21:58:39 -0500

    Working on index.js.

--- models/index.js
+++ models/index.js
@@ -3,7 +3,14 @@ const Planet = require('./planet');
 
 
 // might need to connect users to planets for homeworld belongs to or has many 
+Planet.belongsTo(User,{
+    foreignKey: "planet_id",
+    onDelete: "CASCADE"
+})
 
+Planet.hasMany(User,{
+    foreignKey: "planet_id",
+})
 
 
 
