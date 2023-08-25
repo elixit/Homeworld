d03e1db992af5d314f52e6682b825d0d9a66da18
jennifer arevalo <arevalo5007@gmail.com>
2023-08-25 00:15:06 -0500

    add home routes

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
+const router = require('../controllers');
+const { route } = require('../controllers');
+const withAuth = require('../utils/auth');
 const User = require('./User'); 
 const Planet = require('./planet');
 
 
 // might need to connect users to planets for homeworld belongs to or has many 
-
 
 
 
