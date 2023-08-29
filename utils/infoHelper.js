3e011ea7c48686f489e022123a65eb22c38f8e9f
jennifer arevalo <arevalo5007@gmail.com>
2023-08-29 13:51:07 -0500

    made info helper js

---
+++ utils/infoHelper.js
@@ -0,0 +1,25 @@
+const Planet = require('../models/planet'); 
+const router  = require("express").Router();
+
+router.get('/:id', async (req ,res)=> {
+     try {
+        let planetInfo = await Planet.findByPk(req.params.id, {
+            attributes: [
+                'planet_name',
+                'planet_size',
+                'planet_distance',
+                'planet_temperature',
+                ' number_moons'
+            ]
+        })
+         planetData = planetInfo.get({ plain: true});
+     } catch (error) {
+        console.log(error);
+        
+     }
+     
+});
+
+const planetData = planetInfo.get({ plain: true});
+
+module.exports = planetData;
